df5e4cdea2c9481e147cd5c4e9db0521a11376cd
Luis Fernando Peres Pires <87454966+LFernandoPP@users.noreply.github.com>
2021-07-29 20:01:11 -0300

    Aula 13 Finalizada (Estrutura Condicional ELSE IF)

--- src/academy/devdojo/maratonajava/introducao/projeto05/Aula05EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonajava/introducao/projeto05/Aula05EstruturasCondicionais01.java
@@ -1,11 +1,13 @@
 package academy.devdojo.maratonajava.introducao.projeto05;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args){
         int idade = 15;
         boolean isAutirzadoComprarBebida = idade >= 18;
-        if (isAutirzadoComprarBebida) {
+        if (isAutirzadoComprarBebida == false) {
             System.out.println("Autorizado a comprar bebida");
+        } else {
+            System.out.println("Não autorizado a comprar bebida");
         }
         if (!isAutirzadoComprarBebida) {
             System.out.println("Não autorizado a comprar bebida");
