71edaa236f486cdfe69ce1097242d5841b53a256
Luis Fernando Peres Pires <87454966+LFernandoPP@users.noreply.github.com>
2021-07-29 13:46:41 -0300

    Aula 08 finalizada

--- src/academy/devdojo/maratonajava/introducao/projeto04/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/projeto04/Aula04Operadores.java
@@ -8,5 +8,17 @@ public class Aula04Operadores {
         double resultado = numero01 / numero02;
         //System.out.println(numero02+numero01+"sas"+numero02+numero01);
         System.out.println(resultado);
+        int resto = 21 % 7;
+        System.out.println(resto);
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte "+isDezIgualVinte);
+        System.out.println("isDezIgualDez "+isDezIgualDez);
+        System.out.println("isDezDiferenteDez "+isDezDiferenteDez);
     }
 }
