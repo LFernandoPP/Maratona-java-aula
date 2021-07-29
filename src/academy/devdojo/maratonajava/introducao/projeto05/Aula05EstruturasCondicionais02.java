05f14b03b58c8ffcb74dc936dfdb7a7d33e2322f
Luis Fernando Peres Pires <87454966+LFernandoPP@users.noreply.github.com>
2021-07-29 20:27:21 -0300

    Aula 14 Finalizada (Estrutura Condicional-Operador Ternário)

--- src/academy/devdojo/maratonajava/introducao/projeto05/Aula05EstruturasCondicionais02.java
+++ src/academy/devdojo/maratonajava/introducao/projeto05/Aula05EstruturasCondicionais02.java
@@ -1,10 +1,8 @@
 package academy.devdojo.maratonajava.introducao.projeto05;
-
 public class Aula05EstruturasCondicionais02 {
     public static void main(String[] args) {
         int idade = 12;
         String categoria;
-
         if(idade<15){
             categoria = ("Categoria Infantil");
         }else if(idade >= 15 && idade < 18){
@@ -12,6 +10,7 @@ public class Aula05EstruturasCondicionais02 {
         } else {
             categoria = ("Categoria Adulto");
         }
+        //categoria=idade<15 ? "Categoria Infantil" : idade >= 15 && idade < 18 ? "Categoria Juvenil" : "Categoria Adulto";
         System.out.println(categoria);
     }
 }
---
+++ src/academy/devdojo/maratonajava/introducao/projeto05/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao.projeto05;
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        double salario = 6000;
+        String resultado = salario > 5000 ? "Eu vou doar 500 pro DevDojo" : "Ainda não tenho condições, mas vou ter!";
+        System.out.println(resultado);
+        //String mensagemDoar = "Eu vou doar 500 pro DevDojo";
+        //String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter!";
+        // (condicao) ? verdadeiro : falso;
+
+        //String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+
+        /* String resultado;
+        if(salario>5000){
+            resultado=mensagemDoar;
+        }else{
+            resultado=mensagemNaoDoar;
+        }  */
+        //System.out.println(salario > 5000 ? mensagemDoar : mensagemNaoDoar);
+        //System.out.println(resultado);
+    }
+}
