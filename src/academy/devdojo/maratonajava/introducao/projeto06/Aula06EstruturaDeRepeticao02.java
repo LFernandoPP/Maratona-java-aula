96e696937e96c1598ead69500b46f0b913c99e07
Luis Fernando Peres Pires <87454966+LFernandoPP@users.noreply.github.com>
2021-07-30 13:29:21 -0300

    Aula 19 Finalizada (Estrutura de Repetição - Exercicio)

---
+++ src/academy/devdojo/maratonajava/introducao/projeto06/Aula06EstruturaDeRepeticao02.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao.projeto06;
+
+public class Aula06EstruturaDeRepeticao02 {
+    public static void main(String[] args) {
+        for (int i=1;i<=1000000;i++)
+           if (i%2==0){
+               System.out.println(i+" Par");
+           }else{
+               System.out.println(i+" Impar");
+           }
+    }
+}
