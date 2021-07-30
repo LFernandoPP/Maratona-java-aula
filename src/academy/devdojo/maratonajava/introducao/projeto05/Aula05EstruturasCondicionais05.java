4a5190105823505363439ce4a7c5d099f65ae51c
Luis Fernando Peres Pires <87454966+LFernandoPP@users.noreply.github.com>
2021-07-29 21:57:52 -0300

    Aula 15 Finalizada (Estrutura Condicional Switch)

---
+++ src/academy/devdojo/maratonajava/introducao/projeto05/Aula05EstruturasCondicionais05.java
@@ -0,0 +1,48 @@
+package academy.devdojo.maratonajava.introducao.projeto05;
+
+public class Aula05EstruturasCondicionais05 {
+    public static void main(String[] args) {
+        byte dia = 10;
+        // char, int, byte, shor, enum, String
+
+        switch (dia) {
+            default:
+                System.out.println("Opção inválida");
+                break;
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("segunda");
+                break;
+            case 3:
+                System.out.println("terça");
+                break;
+            case 4:
+                System.out.println("quarta");
+                break;
+            case 5:
+                System.out.println("quinta");
+                break;
+            case 6:
+                System.out.println("sexta");
+                break;
+            case 7:
+                System.out.println("sabado");
+                break;
+        }
+        String sexo = "F";
+        switch (sexo){
+            default:
+                System.out.println("Inválido");
+                break;
+            case "M":
+                System.out.println("Homem");
+                break;
+            case "F": {
+                System.out.println("Mulher");
+                break;
+            }
+        }
+    }
+}
