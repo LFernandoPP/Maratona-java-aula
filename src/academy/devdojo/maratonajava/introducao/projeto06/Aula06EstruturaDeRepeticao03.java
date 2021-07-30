c74eca8c618cd79a3e35aa31b67991756d3c8bc1
Luis Fernando Peres Pires <87454966+LFernandoPP@users.noreply.github.com>
2021-07-30 14:01:52 -0300

    rename

--- src/academy/devdojo/maratonajava/introducao/projeto06/Aula06EstruturaDeRepeticao03.java
+++ src/academy/devdojo/maratonajava/introducao/projeto06/Aula06EstruturaDeRepeticao03.java
@@ -1,5 +1,5 @@
 package academy.devdojo.maratonajava.introducao.projeto06;
-public class Aula06EstruturaDeRepeticao06 {
+public class Aula06EstruturaDeRepeticao03 {
     public static void main(String[] args) {
     int valorMax = 50;
         for (int i = 1; i <= valorMax; i++) {
