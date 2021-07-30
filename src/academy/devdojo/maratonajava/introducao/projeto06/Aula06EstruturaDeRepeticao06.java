4ef0b7d61261dd4b115ba84b2ba57c7dbb327d27
Luis Fernando Peres Pires <87454966+LFernandoPP@users.noreply.github.com>
2021-07-30 13:56:33 -0300

    Aula 20 Finalizada (Estrutura de Repetição - BREAK)

---
+++ src/academy/devdojo/maratonajava/introducao/projeto06/Aula06EstruturaDeRepeticao06.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao.projeto06;
+public class Aula06EstruturaDeRepeticao06 {
+    public static void main(String[] args) {
+    int valorMax = 50;
+        for (int i = 1; i <= valorMax; i++) {
+            if (i>25){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
