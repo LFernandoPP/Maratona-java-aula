ec76d92fbcdd58913c1f9169fddf5b6da6664e9d
Luis Fernando Peres Pires <87454966+LFernandoPP@users.noreply.github.com>
2021-07-31 12:54:15 -0300

    Aula 27 Finalizada (Array-Multidimensionais-Foreach)

--- src/academy/devdojo/maratonajava/introducao/projeto06/Aula08ArrayMultidimensionais01.java
+++ src/academy/devdojo/maratonajava/introducao/projeto06/Aula08ArrayMultidimensionais01.java
@@ -9,10 +9,15 @@ public class Aula08ArrayMultidimensionais01 {
         dias[1][1] = 28;
         dias[1][2] = 31;
         for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < dias[0].length; j++) {
+            for (int j = 0; j < dias[i].length; j++) {
                 System.out.println(dias[i][j]);
             }
-        }{
+        }
+        System.out.println("___________________");
+        for (int[] arrBase: dias){
+            for (int num: arrBase){
+                System.out.println(num);
+            }
         }
     }
 }
