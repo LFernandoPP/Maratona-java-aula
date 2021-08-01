e973f71cba43a364c14df6360c5f3326c8671182
Luis Fernando Peres Pires <87454966+LFernandoPP@users.noreply.github.com>
2021-08-01 16:03:22 -0300

    Aula 34 Finalizada (Métodos-Parâmetros)

---
+++ src/academy/devdojo/maratonajava/javacore/introducao/metodos/projeto01/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.introducao.metodos.projeto01.test;
+
+import academy.devdojo.maratonajava.javacore.introducao.metodos.projeto01.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora=new Calculadora();
+        calculadora.multiplicaDoisNumeros(10,20);
+    }
+}
